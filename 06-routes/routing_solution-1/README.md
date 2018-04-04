4a6b9ee51aef1621434752d8dfb9dac354b30128
marcosecchi <loziosecchi@gmail.com>
2018-04-04 16:19:16 +0200

    Add routing solution draft.

---
+++ 06-routes/routing_solution-1/README.md
@@ -0,0 +1,17 @@
+# Esercizio: Routing
+
+## Parte 1
+
+Implementare un sistema di _routing_ in modo tale che:
+
+* `RouteConfig` in _*app/routes/app.routes.ts*_
+* Use correct `routerLink` to provide links to Home, Users List and Company List
+* Setup a default route for the `Home` component 
+
+## Parte 2
+
+Completare la pipe _shout_ in modo che:
+
+- Se viene passato un parametro booleano con valore _true_, venga aggiunto un punto esclamativo (_!_) alla fine del messaggio.
+
+## Parte 3
---
+++ 06-routes/routing_solution-1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
