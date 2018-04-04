2b2375e3a0033180aac65fee16c0589be904b6a0
marcosecchi <loziosecchi@gmail.com>
2018-04-04 17:25:04 +0200

    Add routing solution 2 draft.

---
+++ 06-routes/routing_solution-2/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'productlist', component: ProductListComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule {}
