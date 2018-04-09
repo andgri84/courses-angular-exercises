100b0ed6de2f309a9d6661acc6012db25e110212
marcosecchi <loziosecchi@gmail.com>
2018-04-09 23:08:09 +0200

    Add ticker example.

---
+++ 01-components/examples/output/src/app/timer/timer.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { timer } from 'rxjs/observable/timer';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  @Output()
+  tick = new EventEmitter<number>();
+
+  count = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    const timerObservable = timer(1000, 1000);
+    timerObservable.subscribe( (t) => {
+      this.count++;
+      this.tick.emit(this.count);
+    });
+
+  }
+
+}
