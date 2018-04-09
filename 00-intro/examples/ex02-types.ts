995859f55219aa6bcf2c2d32dfe0b36b02b3be4b
marcosecchi <loziosecchi@gmail.com>
2018-04-09 21:10:58 +0200

    Update project strucure.

--- 00-intro/examples/ex02-types.ts
+++ 00-intro/examples/ex02-types.ts
@@ -1,55 +1,65 @@
 console.log("\n--- boolean ---");
-var flag = false;
+let flag: boolean = false;
 console.log(flag);
+
 console.log("\n--- number ---");
-var decimal = 6;
-var hex = 0xf00d;
-var binary = 10;
-var octal = 484;
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
 console.log(decimal);
 console.log(hex);
 console.log(binary);
 console.log(octal);
+
 console.log("\n--- string ---");
-var city = "Milan";
+let city: string = "Milan";
 console.log(city);
-var sentence = "Lorem ipsum dolor sit amet,\n    consetetur sadipscing elitr,\n    sed diam nonumy eirmod tempor invidunt\n    ut labore et dolore magna aliquyam erat,\n    sed diam voluptua.";
+
+let sentence: string = `Lorem ipsum dolor sit amet,
+    consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt
+    ut labore et dolore magna aliquyam erat,
+    sed diam voluptua.`;
 console.log(sentence);
-var fullName = "Marco Secchi";
-sentence = "Hello, my name is " + fullName;
+
+const fullName = "Marco Secchi";
+sentence = `Hello, my name is ${ fullName }`;
 console.log(sentence);
+
 console.log("\n--- array ---");
-var list = [1, 2, 3];
+let list: Array<number> = [1, 2, 3];
 console.log(list);
+
 console.log("\n--- tuple ---");
-var tuple;
+let tuple: [string, number];
 tuple = ["hello", 10];
 console.log(tuple);
+
 console.log("\n--- enum ---");
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-var color = Color.Green;
+enum Color {Red, Green, Blue}
+let color: Color = Color.Green;
 console.log(color);
+
 console.log("\n--- any ---");
-var notSure = 4;
+let notSure: any = 4;
 console.log(notSure);
 notSure = "Lorem Ipsum";
 console.log(notSure);
 console.log(notSure.aValue); // nessun errore in fase di compilazione
+
 console.log("\n--- undefined and null ---");
-var und;
+let und: any;
 console.log(und);
 und = null;
 console.log(und);
+
 // console.log("\n--- void ---");
-function warnUser() {
+ function warnUser(): void {
     alert("This is my warning message");
-}
+ }
+
 // console.log("\n--- never ---");
-function error(message) {
+ function error(message: string): never {
     throw new Error(message);
-}
+ }
