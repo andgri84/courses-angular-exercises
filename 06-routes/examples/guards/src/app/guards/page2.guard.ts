c916c991a67350e11aef9b9def5f6e1edc0cadb8
marcosecchi <loziosecchi@gmail.com>
2018-04-12 07:18:01 +0200

    Improve guards example.

--- 06-routes/examples/guards/src/app/guards/page2.guard.ts
+++ 06-routes/examples/guards/src/app/guards/page2.guard.ts
@@ -1,10 +1,16 @@
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class Page2Guard implements CanActivate {
-  canActivate(): boolean {
-    console.log('[Page3Guard]: cannot activate');
-    return false;
+
+  canActivate(): Observable<boolean> {
+    console.log('[Page2Guard]: will activate in 1000ms');
+
+    return Observable.of(true).delay(1000).do(val => false);
   }
 }
--- 06-routes/examples/guards/src/app/guards/page4.guard.ts
+++ 06-routes/examples/guards/src/app/guards/page4.guard.ts
@@ -1,12 +1,16 @@
 import { Page4Component } from './../components/page4/page4.component';
 import { Injectable } from '@angular/core';
-import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class Page4Guard implements CanDeactivate<Page4Component> {
+export class Page4Guard implements CanDeactivate<Page4Component>, CanActivate {
   canDeactivate(component: Page4Component): boolean {
     console.log('[Page4Guard]: cannot deactivate');
     return false;
+  }
+
+  canActivate(): boolean {
+    return true;
   }
 }
